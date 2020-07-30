43999e8ea539b93d7aa0e1fc75c8a886d6fedf79
Artem Sitnov <stnoov@gmail.com>
2020-07-30 20:01:34 +0300

    Fix 'places' type in meeting creation

--- okiedokie/meetings/forms.py
+++ okiedokie/meetings/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, IntegerField
 from wtforms.validators import DataRequired
 
 
@@ -7,7 +7,7 @@ class CreateEventForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     date = StringField('Date and Time', validators=[DataRequired()])
     duration = StringField('Duration', validators=[DataRequired()])
-    places = StringField('Places', validators=[DataRequired()])
+    places = IntegerField('Places', validators=[DataRequired()])
     text = TextAreaField('Text', validators=[DataRequired()])
     zoom_link = StringField('Zoom', validators=[DataRequired()])
     submit = SubmitField('Create Event')
