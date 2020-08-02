789f8282ac8f075d7f189bfbea68660faea1a01e
Artem Sitnov <stnoov@gmail.com>
2020-08-02 13:51:00 +0300

    Hotfix

--- okiedokie/ya_payments/routes.py
+++ okiedokie/ya_payments/routes.py
@@ -24,7 +24,7 @@ def notification():
     hash2 = request.form['notification_type'] + '&' + request.form['operation_id'] \
             + '&' + request.form['amount'] + '&' + request.form['currency'] + '&' + \
             request.form['datetime'] + '&' + request.form['sender'] + '&' + \
-            request.form['codepro'] + '&' + 'JNISfRip1K8HekP+U0uwyVR3' + '&' + request.form['label']
+            request.form['codepro'] + '&' + 'f9R1OVrbXU1QRv9STHXPNq81' + '&' + request.form['label']
 
     hash = hashlib.sha1(str(hash2).encode('utf-8')).hexdigest()
 
